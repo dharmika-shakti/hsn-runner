5a5b208a455bca655c67d9fc5be53beb78695703
DESKTOP-CMUHFF3\Shakti-240 <dharmika.shaktiwebsolutions@gmail.com>
2026-03-19 12:57:53 +0530

    add test phase

--- test-chrome-path.js
+++ test-chrome-path.js
@@ -3,3 +3,4 @@ import fs from 'fs';
 
 console.log('Executable Path:', puppeteer.executablePath());
 console.log('Exists:', fs.existsSync(puppeteer.executablePath()));
+console.log('Exists chromium:', fs.existsSync('/usr/bin/chromium'));
